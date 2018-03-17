e3bd03a9d711e43e58c365b6d4ffc169e5ebe928
Ozan Kasikci <kasikciozan@yandex.com>
2018-03-17 23:40:29 +0300

    server: errors: add neo errors

--- server/errors.js
+++ server/errors.js
@@ -4,4 +4,7 @@ module.exports = {
   NasaApi: {
     failedToFetchNEORecords: new Error('Failed to fetch NEO records!'),
   },
+  NEO: {
+    failedToFetchHazardousRecords: new Error('Failed to fetch hazardous NEO records!'),
+  },
 };
